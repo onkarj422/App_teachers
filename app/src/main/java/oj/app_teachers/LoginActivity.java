42faa698a08d490c8be350ba174f9137dc433f21
Onkar J <onkarj422@gmail.com>
2017-07-02 13:42:13 +0530

    Initial commit

---
+++ app/src/main/java/oj/app_teachers/LoginActivity.java
@@ -0,0 +1,13 @@
+package oj.app_teachers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

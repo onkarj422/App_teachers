8ef5847347d4651c437910ed05009815d3fcc6b4
karan <karangupta388@gmail.com>
2017-07-07 01:46:59 +0530

    Details:-Simple Login code has been completed and the changes done are commented.

--- app/src/main/java/oj/app_teachers/LoginActivity.java
+++ app/src/main/java/oj/app_teachers/LoginActivity.java
@@ -1,7 +1,10 @@
 package oj.app_teachers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -9,17 +12,41 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText editLogin;
     private EditText editPassword;
+    private Button clickButton;
+    String LoginID="admin";
+    String Password="pass";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        editLogin = (EditText) findViewById(R.id.editText);
-        editPassword = (EditText) findViewById(R.id.editText1);
-    }
+        editLogin = (EditText) findViewById(R.id.LoginID);//r.id changed from edittext to LoginID
+        editPassword = (EditText) findViewById(R.id.Password);//r.id.changed from edittext to Password
+        clickButton = (Button) findViewById(R.id.button);
+        clickButton.setOnClickListener( new View.OnClickListener() {
 
-    public void login() {
-        String loginId = editLogin.getText().toString().toLowerCase();
-        String password = editPassword.getText().toString().toLowerCase();
+
+                                            @Override
+                                            public void onClick(View view) {
+                                                String loginId = editLogin.getText().toString().toLowerCase();
+                                                String password = editPassword.getText().toString().toLowerCase();
+                                                if (LoginID.equals(loginId) && Password.equals(password)) {
+                                                    Toast.makeText(LoginActivity.this, "Logged in successfully", Toast.LENGTH_LONG).show();
+                                                   Intent i=new Intent(LoginActivity.this,SecondActivity.class);
+                                                    startActivity(i);
+
+                                                } else {
+                                                    Toast.makeText(LoginActivity.this, "Please try again!!!", Toast.LENGTH_LONG).show();
+                                                }
+                                            }
+    });
     }
 }
+
+            //public void login() {
+
+            //}
+        //}
+    //}
+//}
+

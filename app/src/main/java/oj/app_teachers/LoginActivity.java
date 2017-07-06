23df1020af222fbbb2238a3aa717f1beb3ee0f32
Onkar J <onkarj422@gmail.com>
2017-07-06 21:06:25 +0530

    Login page created. Please use string variables declared in activity code.

--- app/src/main/java/oj/app_teachers/LoginActivity.java
+++ app/src/main/java/oj/app_teachers/LoginActivity.java
@@ -2,14 +2,24 @@ package oj.app_teachers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editLogin;
+    private EditText editPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toast.makeText(LoginActivity.this,"Coding", Toast.LENGTH_LONG).show();
+        editLogin = (EditText) findViewById(R.id.editText);
+        editPassword = (EditText) findViewById(R.id.editText1);
+    }
+
+    public void login() {
+        String loginId = editLogin.getText().toString().toLowerCase();
+        String password = editPassword.getText().toString().toLowerCase();
     }
 }
